3d9cf134f77b29a6f97effaeaf1037408823533b
Joshua Raphael Espeso <132674507+Anakin1896@users.noreply.github.com>
2026-04-21 21:29:55 +0800

    Update accounts

--- gradesync_backend/accounts/admin.py
+++ gradesync_backend/accounts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, UserSettings
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('School Profile', {
+            'fields': (
+                'title_prefix', 
+                'middle_initial', 
+                'employee_id', 
+                'role', 
+                'position_title', 
+                'department', 
+                'school_name'
+            )
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('School Profile', {
+            'fields': (
+                'title_prefix', 
+                'middle_initial', 
+                'employee_id', 
+                'role', 
+                'position_title', 
+                'department', 
+                'school_name'
+            )
+        }),
+    )
+
+    list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'department']
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserSettings)
