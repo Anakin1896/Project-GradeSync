940fe0808af5f846ffc9a349e08cc85eb1e37061
Joshua Raphael Espeso <132674507+Anakin1896@users.noreply.github.com>
2026-05-12 11:28:35 +0800

    Update

--- gradesync_frontend/src/main.jsx
+++ gradesync_frontend/src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+
+    console.log("New content available, please refresh.");
+  },
+  onOfflineReady() {
+    console.log("GradeSync is ready to work offline.");
+  },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
